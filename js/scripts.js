1b8a996925c689c0c52c48cc3d5abf06d1acecb4
Allison Sadin <aesadin@gmail.com>
2020-06-08 11:50:25 -0700

    create function that turns variables into one array, then seperate array into new arrays and create a list of those items after turning them into strings
    Co-authored-by: Lizzie Van Hook <evanhook360@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,20 @@ $(document).ready(function() {
     const movieInput = $("input#movie").val();
     const foodInput = $("input#food").val();
     const songInput = $("input#song").val();
+    const allMyFavoriteThings = [movieInput, foodInput, songInput];
 
+    $("#allSpan").text(allMyFavoriteThings.toString());
     $("#movieSpan").text(movieInput);
     $("#foodSpan").text(foodInput);
     $("#songSpan").text(songInput);
 
-    //const allMyFavoriteThings = ["movieInput", "foodInput", "songInput"]
+  const newMovie = [allMyFavoriteThings[0]];
+  const newFood = [allMyFavoriteThings[1]]
+  const newSong = [allMyFavoriteThings[2]]
+
+  $("#movie1").text(newMovie.toString());
+  $("#food1").text(newFood.toString());
+  $("#song1").text(newSong.toString());
+    
   });
 });
